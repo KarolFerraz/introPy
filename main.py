c89077bff103ba733f659d898334bac3ab63e206
Karol <karolyneferraz@gmail.com>
2021-10-08 16:59:07 -0300

    Aula 2 do Python - menu com while (FUNCIONAL)

--- main.py
+++ main.py
@@ -66,3 +66,51 @@ if __name__ == '__main__':
     # brincar de plim
     brincar_de_plim(100)
 
+# estrutura de identificação / execução do script
+if __name__ == '__main__':
+
+    resposta = "C"
+
+    while resposta.upper() != 'Z':
+
+        print('#####################################')
+        print('#                                   #')
+        print('#    M E N U   D E   O P Ç Õ E S    #')
+        print('#                                   #')
+        print('#    1 - Olá Mundo                  #')
+        print('#    2 - Área do Retângulo          #')
+        print('#    3 - Área do Quadrado           #')
+        print('#    4 - Área do Triângulo          #')
+        print('#    5 - Contagem Progressiva       #')
+        print('#    6 - Apoiar Candidato           #')
+        print('#    7 - Brincar de Plim            #')
+        print('#                                   #')
+        print('#    Z - Sair                       #')
+        print('#                                   #')
+        print('#####################################')
+
+        resposta = input("Escolha sua opção")
+        print(f'A sua escolha foi: {resposta}')
+
+        if resposta.upper() != 'Z':
+            if resposta == '1':
+                print_hi('José')
+            elif resposta == '2':
+                resultado = calcular_area_do_retangulo(8,7)
+                print(f'A área do retângulo é de {resultado} m²')
+            elif resposta == '3':
+                resultado =calcular_area_do_quadrado(6)
+                print(f'A área do quadrado é de {resultado} m²')
+            elif resposta == '4':
+                resultado = calcular_area_do_triangulo(5,8)
+                print(f'A área do triângulo é de {resultado} m²')
+            elif resposta == '5':
+                contagem_progressiva(10)
+            elif resposta == '6':
+                apoiar_candidato('Murphy',13)
+            elif resposta == '7':
+                brincar_de_plim(7)
+            else:
+                print('Você digitou uma opção inválida. Escolha uma opção de 1 a 7')
+        else:
+            print("Você escolheu sair. Volte Sempre!")
